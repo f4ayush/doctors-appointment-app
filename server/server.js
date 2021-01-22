b038a73f874eabef234b6b4e05cd5bd8c96ea669
Ayush raj anand <rajanand.ayush98@gmail.com>
2021-01-23 00:06:05 +0530

    changes made to use middlewares

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
-const { request, static } = require("express");
+const mysql = require("mysql");
+const fs = require("fs");
+const envVariables = require("./config.js");
+const path = require("path");
+const express = require("express");
+const connection = require("./database");
+const app = express();
+const getAppointment = require("./route/getAppointment");
+const PORT = process.env.PORT || envVariables.port;
+connection;
 
-let express = require("express");
-const { resolve } = require("path");
-let app = express();
-
-app.use(express.static(resolve(__dirname + "/../public")));
-
-app.get("/getAppointment", (req, res) => {
-  console.log("yes");
-  res.send("this is a response");
-});
-app.listen(3000);
+app.use(express.static(path.resolve(__dirname + "/../public")));
+app.use("/register", getAppointment);
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
